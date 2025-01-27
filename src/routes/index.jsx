0f0d2d4191f449bddb684ff5b1d8b95d578482b4
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-01-27 20:43:17 -0300

    :construction: First Commit - Routes já separa com Admin/Customer/Seler

---
+++ src/routes/index.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import { useAuth } from "../hooks/auth";
+
+import { AdminRoutes } from './admin.routes';
+import { AuthRoutes } from './auth.routes';
+
+export function Routes() {
+  const { user } = useAuth();
+
+  return (
+    <BrowserRouter>
+      {user ? <AdminRoutes /> : <AuthRoutes />}
+    </BrowserRouter>
+  );
+}
