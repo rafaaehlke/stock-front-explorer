8de74b6ff2406e3c7e1b59858e6bb11c8fbf1ef3
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-01-27 21:35:25 -0300

    :construction: Ocultando opçoes do menu em base do perfil logado

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,28 @@
 import { FiTruck, FiTag, FiShoppingCart } from 'react-icons/fi';
 
-import { Container } from "./styles";
 import { Feature } from '../../components/Feature';
 import { Header } from '../../components/Header';
+import { useAuth } from '../../hooks/auth';
+import { Container } from "./styles";
+import { USER_ROLE } from '../../utils/role'
 
 export function Home() {
+  const { user } = useAuth()
+
   return (
     <Container>
       <Header />
 
       <main>
         <Feature title="Produto" icon={FiTag} to="/product" />
-        <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
-        <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+        
+         {/* Exibe a seção de Fornecedores apenas para administradores */}
+        {[USER_ROLE.ADMIN].includes(user.role) && < Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />} 
+        
+        {/*  Exibe o relatório de vendas para administradores e clientes */}
+        {[USER_ROLE.ADMIN, USER_ROLE.SALE].includes(user.role) && <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />}
+
       </main>
-    </Container>
+    </Container >
   )
 }
