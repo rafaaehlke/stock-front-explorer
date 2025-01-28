0327abe7c90f2a86517c23d8fcd068a5dd00c9e5
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-01-27 21:09:14 -0300

    :construction: Criado Switch verificando qual perfil de usuario esta sendo logado

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,33 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import { useAuth } from "../hooks/auth";
+import { USER_ROLE } from "../utils/role"
 
 import { AdminRoutes } from './admin.routes';
 import { AuthRoutes } from './auth.routes';
+import { CustomerRoutes } from './customer.routes';
+import { SaleRoutes } from './sale.routes';
+
 
 export function Routes() {
   const { user } = useAuth();
 
+  function AcessRoute() {
+    switch (user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes />
+      case USER_ROLE.SALE:
+        return <SaleRoutes />
+
+      default:
+        return <CustomerRoutes />
+    }
+  }
   return (
     <BrowserRouter>
-      {user ? <AdminRoutes /> : <AuthRoutes />}
+      {user ? <AcessRoute /> : <AuthRoutes />}
     </BrowserRouter>
   );
 }
