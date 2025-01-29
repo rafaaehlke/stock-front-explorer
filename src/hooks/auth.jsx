f1879565532125b3bbcd3d43951786d56fc86282
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-01-28 23:46:34 -0300

    :recycle: Refatorado para receber o cookie do backend

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -14,15 +14,16 @@ function AuthProvider({ children }) {
 
   async function signIn({ email, password }) {
     try {
-      const response = await api.post("sessions", { email, password });
-      const { token, user } = response.data;
+      const response = await api.post(
+        "sessions",
+        { email, password }, // payload, dados que enviam para a rota
+        { withCredentials: true } // front troca com o backend, cookie que possui token
+      );
+      const { user } = response.data;
 
       localStorage.setItem("@estock:user", JSON.stringify(user));
-      localStorage.setItem("@estock:token", token);
 
-      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-
-      setData({ token, user });
+      setData({ user });
 
     } catch (error) {
       if (error.response) {
@@ -34,7 +35,6 @@ function AuthProvider({ children }) {
   };
 
   function signOut() {
-    localStorage.removeItem("@estock:token");
     localStorage.removeItem("@estock:user");
 
     setData({});
@@ -42,14 +42,10 @@ function AuthProvider({ children }) {
 
 
   useEffect(() => {
-    const token = localStorage.getItem("@estock:token");
     const user = localStorage.getItem("@estock:user");
 
-    if (token && user) {
-      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-
+    if (user) {
       setData({
-        token,
         user: JSON.parse(user)
       });
     }
