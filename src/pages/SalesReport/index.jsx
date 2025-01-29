a5c07c6f25a91106b1cf6f54f67c859af201c4f6
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-01-29 00:21:09 -0300

    :recycle: utilizamos o useEffect para fazer uma requisição à rota de vendas

--- src/pages/SalesReport/index.jsx
+++ src/pages/SalesReport/index.jsx
@@ -1,6 +1,12 @@
+import { api } from "../../services/api"
 import { Container } from "./styles";
+import { useEffect } from "react";
+
 
 export function SalesReport() {
+  useEffect(() => {
+    api.get('/sales', { withCredentials: true }).then((response) => console.log(response.data))
+  }, [])
   return (
     <Container>
       <h1>Relatório de Vendas</h1>
